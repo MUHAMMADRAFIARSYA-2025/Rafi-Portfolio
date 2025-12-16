6d44ebe3165694bb098b9fcdd6109f863a2038c5
MUHAMMADRAFIARSYA-2025 <24203769@siswa.um.edu.my>
2025-12-16 12:42:22 +0800

    Fix untuk Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio/",
+  base: "/Rafi-Portfolio/",
   plugins: [react(), tailwindcss()],
 })
